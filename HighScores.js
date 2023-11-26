fd6119fdda01f69d4da20a8b7e155fdffd61d89b
Maritza Diaz <maritzadiaz77@gmail.com>
2023-11-26 02:36:42 -0800

    Modified the javascript and connected my html to my second javascript file. Modified the css.

---
+++ HighScores.js
@@ -0,0 +1,26 @@
+function displayHighscores() {
+  // Here we get scores from local storage OR empty array in empty brackets.
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+  //refer to web apis activities for this part
+  highscores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  highscores.forEach(function (score) {
+    //  here we want to create an li tag for every recorded score.
+    var newTag = document.createElement("li");
+    newTag.textContent = score.initials + " - " + score.score;
+
+    // list on the page!
+    var olEl = document.querySelector("#highscores");
+    olEl.appendChild(newTag);
+  });
+}
+function clearHighscores() {
+  window.localStorage.removeItem("highscores");
+  window.location.reload();
+}
+document.getElementById("clear").addEventListener("click", clearHighscores);
+//run function when page loads
+displayHighscores();
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// These are my list of questions that I am going to be asking.
+// These are my list of questions that I am going to be asking.This is the easiest and 
+// quickest part.
 var questions=[
     {
         title: "Commonly used data types DO Not Include:",
@@ -51,3 +52,160 @@ var questions=[
         answer: "4. console.log"
 },
 ];
+// In this section, I am going to be going through the DOCUMENT to get to the #items below.
+var timerEl = document.querySelector("#time");
+var startBtn = document.querySelector("#start");
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+// These are the variables for the questions
+var activeQuestionIndex = 0;
+var secondsLeft = questions.length * 10;
+var setTimer;
+// This is the function for startquiz
+function startQuiz() {
+
+    
+            // Here we are hiding the starting page we began with.
+            var startInfoEl = document.querySelector("#start-page");
+            startInfoEl.setAttribute("class", "hide");
+            // showing my question elements
+            questionsEl.removeAttribute("class");
+           // SetTImer is setting the timer, has 2 parameters below in parenthesis.
+           setTimer = setInterval(setTime, 1000);
+            // This is going throuhg the timer element, its content, and saying how 
+            // many seconds left.
+            timerEl.textContent = secondsLeft;
+            startQuestion();
+       }     
+function startQuestion() {
+
+    // get first question
+    var activeQuestion = questions[activeQuestionIndex];
+
+    // update title with active question
+    var titleEl = document.querySelector("#title")
+    titleEl.textContent = activeQuestion.title;
+
+    // clear previous choices
+    choicesEl.innerHTML = "";
+
+    // loop over choices    
+    activeQuestion.choices.forEach(function(choice) {
+
+        // creat button for each choice
+        var choiceButton = document.createElement("button");
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+
+        choiceButton.textContent = choice;
+
+        // click event for each choice
+        choiceButton.addEventListener("click", choiceClick);
+
+        // disply on the page
+        choicesEl.appendChild(choiceButton);
+    });
+}
+
+function choiceClick() {
+    if (this.value !== questions[activeQuestionIndex].answer) {
+        // diduct time
+        secondsLeft -= 15;
+
+        if(secondsLeft < 0) {
+            secondsLeft = 0;
+        }
+        // How many second left on the page.
+        // if correct, then the text will come out green.
+        // if incorrect, then the text will be red
+        timerEl.textContent = secondsLeft;
+        feedbackEl.textContent = "Wrong!";
+        feedbackEl.style.color = "red";
+        feedbackEl.style.fontSize = "300%";
+    } else {
+        feedbackEl.textContent = "Correct!";
+        feedbackEl.style.color = "green";
+        feedbackEl.style.fontSize = "300%";
+    }
+
+    // displying coorect/wrong 
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    // This ++ means that it will show the next question.
+    activeQuestionIndex++;
+   
+    if (activeQuestionIndex === questions.length) {
+        endQuestion();        
+    } else {
+        startQuestion();
+    }
+}
+
+// function to end the quiz
+// Need to ask tutor for help with this part.
+function endQuestion() {
+    clearInterval(setTimer);
+    var inputEl = document.querySelector('#save-score');
+    inputEl.removeAttribute("class");
+
+    var finalScoreEl = document.querySelector("#final-score");
+    finalScoreEl.textContent = secondsLeft;
+
+    // hide questions section
+    questionsEl.setAttribute("class", "hide");
+}
+
+function setTime() {
+
+    secondsLeft --;
+    timerEl.textContent = secondsLeft;    
+
+    if (secondsLeft <= 0) {
+       endQuestion();               
+    }
+
+// Here we are defining the function of savehighscore.
+function saveHighscore() {
+// we can check this on inspect.
+    var initials = initialsEl.value.trim();
+
+    if (initials !== "") {
+        // breaking down
+        // going through window, then to the localstorage to .getitem
+        var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        // New score for whoever last played and filled in initials
+        var newScore = {
+            score: secondsLeft,
+            initials: initials
+        };
+
+        // essentially what this is doing is saving the latest score on our localstorage
+        
+        highscores.push(newScore);
+        // and we are able to check through inspect
+        window.localStorage.setItem("highscore", JSON.stringify(highscores));
+    
+        //  This is taking us to our other html page besides the main.
+        window.location.href = "ViewScores.html";
+    }
+}
+// adding an event lister for when key is pressed!
+initialsEl.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
+});
+
+//type in your intitials to record your high score!
+submitBtn.addEventListener("click", saveHighscore);
+
+// adding the click event to the button!
+startBtn.addEventListener("click", startQuiz);
