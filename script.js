ab2f3b16e3868ba68dbaaae1d8216f8f19825169
Maritza Diaz <maritzadiaz77@gmail.com>
2023-11-26 01:04:23 -0800

    Added all of my questions on script.

---
+++ script.js
@@ -0,0 +1,53 @@
+// These are my list of questions that I am going to be asking.
+var questions=[
+    {
+        title: "Commonly used data types DO Not Include:",
+        choices: [
+            "1. strings",
+            "2. booleans",
+            "3. alerts",
+            "4. numbers"
+        ],
+        answer: "3. alerts"
+    },
+    {
+    title: "The condition in an if/else statement is enclosed with___.",
+        choices: [
+            "1. quotes",
+            "2. curly brackets",
+            "3. parenthesis",
+            "4. square brackets"
+        ],
+        answer: "3. parenthesis"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ___.",
+        choices: [
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above"
+        ],
+        answer: "4. all of the above"
+    },
+{
+    title: "Sring values must be enclosed within ___ when being assigned to variables.",
+        choices: [
+            "1. commas",
+            "2. curly brackets",
+            "3. quotes",
+            "4. parenthesis"
+        ],
+        answer: "3. quotes"
+    },
+{
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: [
+            "1. Javascript",
+            "2. terminal/bash",
+            "3. for loops",
+            "4. console.log"
+        ],
+        answer: "4. console.log"
+},
+];
